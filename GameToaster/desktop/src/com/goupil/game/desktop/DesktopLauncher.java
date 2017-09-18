bf56d76c0cbb9da204bc9340a8fe11529e0e99e8
renardn <renardn@WEMFB3201.student.fr.ch>
2017-09-18 13:48:44 +0200

    Changed Engine

---
+++ GameToaster/desktop/src/com/goupil/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.goupil.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.goupil.game.GameToasterRun;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameToasterRun(), config);
+	}
+}
--- GameToaster/src/main/java/goupil/game/Start.java
+++
@@ -1,9 +0,0 @@
-package goupil.game;
-
-public class Start {
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-    }
-}
